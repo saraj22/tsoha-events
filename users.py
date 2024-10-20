19a5bcb1fc6f3d00be52424cfa159c80220dfaa6
Sara <sara.jaakola@gmail.com>
2024-10-20 22:22:28 +0300

    Update users.py

--- users.py
+++ users.py
@@ -1,3 +1,4 @@
+import os
 from db import db
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -13,6 +14,7 @@ def login(username, password):
     else:
         if check_password_hash(user.password, password):
             session["user_id"] = user.id
+            session["csrf_token"] = os.urandom(16).hex()
             return True
         else:
             return False
@@ -33,4 +35,46 @@ def register(username, password):
         return False
     return login(username, password)
 
+def check_csrf(csrf_token):
+    if session["csrf_token"] != csrf_token:
+        abort(403)
 
+
+
+
+
+def get_username(user_id):
+    sql = text("SELECT username FROM users WHERE id=:user_id")
+    return db.session.execute(sql, {"user_id": user_id}).fetchone()[0]
+
+def get_user_myevents(user_id):
+    sql = text("SELECT * FROM events WHERE user_id=:user_id")
+    return db.session.execute(sql, {"user_id": user_id}).fetchall()
+
+def get_user_friends(user_id):
+    sql = text("SELECT u.id, u.username FROM friends f JOIN users u ON f.friend_id=u.id WHERE f.user_id=:user_id")
+    return db.session.execute(sql, {"user_id": user_id}).fetchall()
+    
+
+def get_friend_state(user_id, friend_id):
+    sql = text("SELECT * FROM friends WHERE user_id=:user_id AND friend_id=:friend_id")
+    result = db.session.execute(sql, {"user_id": user_id, "friend_id": friend_id})
+    state = result.fetchall()
+
+    if not state: return 0
+    else: return 1
+
+
+
+
+def follow_user(user_id, friend_id):
+    sql = text("INSERT INTO friends (user_id, friend_id) VALUES (:user_id, :friend_id)")
+    db.session.execute(sql, {"user_id":user_id, "friend_id":friend_id})
+    db.session.commit()
+    return
+
+def unfollow_user(user_id, friend_id):
+    sql = text("DELETE FROM friends WHERE user_id=:user_id AND friend_id=:friend_id")
+    db.session.execute(sql, {"user_id":user_id, "friend_id":friend_id})
+    db.session.commit()
+    return
